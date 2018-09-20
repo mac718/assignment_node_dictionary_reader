bc3fa0a5b86dcaa37e6238544cf3665599e0c745
mac718 <mac718@gmail.com>
2018-09-19 22:52:11 -0400

    Refactor loader.scanDir to accomodate async readdir

--- dictionary_reader/loader.js
+++ dictionary_reader/loader.js
@@ -2,13 +2,14 @@ var fs = require('fs');
 //var ui = require('./ui');
 
 var loader = {
-  scanDir: (dir) => {
+  scanDir: (dir, callback) => {
     //let dictionaryArray = [];
-    return fs.readdirSync(dir, (err, data) => {
+    fs.readdir(dir, (err, data) => {
       if (err) throw err;
+      callback(data);
       //console.log(data);
       //dictionaryArray.then(result => { ui.listDictionaries(result) });
-    }).filter(file => {return file.match(/.json/)});
+    });
     //return dictionaryArray;
   }
 }
--- dictionary_reader/ui.js
+++ dictionary_reader/ui.js
@@ -42,16 +42,30 @@ var ui = {
   },
 
   selectDictionary: () => {
-    let dictionaries = loader.scanDir('./data');
-    ui.listDictionaries(dictionaries);
+    let dictionaries = [];
+    loader.scanDir('./data', (data) => {
+      dictionaries = data.filter(file => {return file.match(/.json/)});
+      ui.listDictionaries(dictionaries);
+      console.log(dictionaries.length);
+    });
+    // loader.scanDir('./data').then(result => {
+    //   result = result.filter(file => {return file.match(/.json/)});
+    //   dictionaries = result;
+    //   ui.listDictionaries(dictionaries);
+    //   console.log(result);
+    // } );
+    //console.log(dictionaries);
+    //ui.listDictionaries(dictionaries);
     process.stdin.resume();
     process.stdin.setEncoding('utf8');
     process.stdin.on('data', data => {
       data = data.trim();
+      
 
       if (data === 'q') {
         process.exit();
       } else if (data > 0 && data <= dictionaries.length) {
+        console.log(data);
         dictionary.displayStats(dictionaries[data - 1]);
       } else {
         console.log('Invaid entry. Try again:');
